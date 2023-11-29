63bc1f74b09e2a5bf6f30ea1520562bb28be0e4e
Katona Valentin Fábió (SZF_2022_2024) <katona.valentin.fabio@diak.szamalk-szalezi.hu>
2023-11-29 13:30:27 +0100

    osszSuly + osszGombSuly metodus megírva

--- src/main/Idomok.java
+++ src/main/Idomok.java
@@ -5,34 +5,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Idomok {
+
     private List<Idom> idomList;
 
     public Idomok() {
         idomList = new ArrayList<>();
     }
-    
-    public double osszSuly(){
-        double oSuly;
-        oSuly = 0.0;
+
+    public double osszSuly() {
+        double oSuly = 0;
+        for (Idom idom : idomList) {
+            oSuly += idom.suly();
+        }
         return oSuly;
     }
-    
-    public double osszGombSuly(){
-        double oGSuly;
-        oGSuly = 0.0;
+
+    public double osszGombSuly() {
+        double oGSuly = 0;
+        for (Idom idom : idomList) {
+            if (idom instanceof idomok.Gomb) {
+                oGSuly += idom.suly();
+            }
+        }
         return oGSuly;
     }
     
-    public Idom minTerfogat(){
-        
+
+    public Idom minTerfogat() {
+
     }
-    
-    public Idom maxTerfogat(){
-        
+
+    public Idom maxTerfogat() {
+
     }
-    
-    public void run(){
-        
+
+    public void run() {
+
     }
-    
+
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -2,22 +2,6 @@ package main;
 public class Main {
     
     public static void main(String[] args) {
-        run();
+        Idomok idomok = new Idomok();
     }
-    public static void run(){
-        Main mn = new Main();
-        mn.legnagyobb();
-        mn.legkisebb();
-       }
-    
-    private void legkisebb(){
-        
-    }
-    
-    private void legnagyobb(){
-        
-    }
-    
-    
-    
 }
